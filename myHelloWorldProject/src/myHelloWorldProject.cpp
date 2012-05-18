cd6e08675b0037b07784de6a303535575c15791f
bernhard <bernhard@vostro1500-fortuneTiger>
2012-05-18 17:30:01 +0200

    cascada commit 2

--- myHelloWorldProject/src/myHelloWorldProject.cpp
+++ myHelloWorldProject/src/myHelloWorldProject.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Cascada!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Cascada. You are the best!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
